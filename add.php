2b0ebdb499a7258cb09afe153fc4c2dde927b2e1
Alex <sashaikonnikov55@gmail.com>
2021-04-19 22:41:50 +0700

    Вывел динамически табы категорий. Немного изменил шаблон, чтобы форма соответствовала активному табу.

--- add.php
+++ add.php
@@ -1,3 +1,32 @@
 <?php
+require('helpers.php');
+require('utils.php');
+
+/**
+ * Получает тип контента из базы данных
+ * @param object ресурс соединения с базой данных
+ * @return array массив типов постов
+ */
+function getTypeContent($mysqli)
+{
+    $sql = "SELECT * FROM type_contents";
+    $result = $mysqli->query($sql);
+    $result = $result->fetch_all(MYSQLI_ASSOC);
+    return $result;
+}
+
+$mysqli = connect();
+$type_content = getTypeContent($mysqli);
+
+$content = include_template('adding-post.php', ['types' => $type_content]);
+
+$data = [
+    'content' => $content,
+    'user_name' => 'Alex',
+    'is_auth' => 1,
+    'page_name' => 'Добавить пост'
+];
+
+print_r(include_template('layout.php', $data));
 
 ?>
